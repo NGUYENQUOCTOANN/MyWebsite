7510f6cfa628086793b1c3eaa950b17f338cc001
NGUYENQUOCTOANN <151893240+NGUYENQUOCTOANN@users.noreply.github.com>
2024-06-10 09:19:44 +0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,10 @@
+document.getElementById('add-to-cart').addEventListener('click', function() {
+    var notification = document.getElementById('notification');
+    notification.textContent = 'Đã mua thành công đơn hàng!';
+    notification.style.display = 'block';
+
+    // Thay đổi nội dung của nút sau khi nhấn
+    this.textContent = 'Mua thành công';
+    this.disabled = true; // Vô hiệu hóa nút sau khi nhấn
+});
 
